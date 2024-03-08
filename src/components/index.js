65f0910cfdd85cdafb324ea6072532be57a1ffa5
babyo77 <144552425+babyo77@users.noreply.github.com>
2024-03-09 02:06:48 +0530

    new

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-import Navbar from "./Navbar";
+import Navbar from "./NavBar";
 import Billing from "./Billing";
 import CardDeal from "./CardDeal";
 import Business from "./Bussiness";
@@ -8,7 +8,7 @@ import Stats from "./Stats";
 import Footer from "./Footer";
 import Testimonials from "./Testimonials";
 import Hero from "./Hero";
-import GetStarted from './GetStarted'
+import GetStarted from "./GetStarted";
 import FeedbackCard from "./FeedBackCard";
 
 export {
@@ -23,5 +23,5 @@ export {
   Testimonials,
   Hero,
   GetStarted,
-  FeedbackCard
+  FeedbackCard,
 };
